bedc3566f2cbe5d7e6a6e9b6800cac01350b48d6
Katarína Brezániová <katkabrezani@gmail.com>
2025-04-03 11:09:07 +0200

    added routing for subscriptions

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,11 @@ const routes = [
     name: 'Home',
     component: HomeView
   },
+  {
+    path: '/',
+    name: 'subscriptions',
+    component: () => import('../views/SubscriptionView.vue'),
+  },
 ];
 
 // Create the router instance
