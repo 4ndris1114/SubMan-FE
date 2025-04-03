8c33a199640748e064970dda5e8bed5cb2db086b
Katarína Brezániová <katkabrezani@gmail.com>
2025-04-03 12:37:44 +0200

    created Interface for User and mapper to map subscriptions and users

---
+++ src/utils/mappers.ts
@@ -0,0 +1,22 @@
+import type { IUser } from "@/types/interfaces/IUser";
+import type { ISubscription } from "@/types/interfaces/ISubscription";
+
+export const mapToIUser = (user: any): IUser => {
+    return {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        createdAt: new Date(user.createdAt), // Ensuring it's a Date object
+    };
+};
+
+export const mapToISubscription = (subscription: any): ISubscription => {
+    return {
+        id: subscription.id,
+        name: subscription.name,
+        description: subscription.description,
+        price: subscription.price,
+        createdAt: new Date(subscription.createdAt),
+        updatedAt: new Date(subscription.updatedAt),
+    };
+};
