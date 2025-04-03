ff03983141e507e8f8954db24db9b8e304b5e013
Katarína Brezániová <katkabrezani@gmail.com>
2025-04-03 11:36:31 +0200

    added upcoming payments to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <template>
-      <div class="min-h-screen bg-gray-50">
-    <Navbar />
+    <div class="min-h-screen bg-gray-50">
+        <Navbar />
+        <div class="p-4 ml-40">
+            <UpcomingPayment />
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import Navbar from '@/components/Navbar.vue'
+import UpcomingPayment from '@/components/UpcomingPayment.vue';
 </script>
 
 <style scoped>
